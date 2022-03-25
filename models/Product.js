13eed8e70ff5c25f3b89a51aa99e89ec4c2adda5
SLEEPlESS <rosas_u@ymail.com>
2022-03-25 16:57:03 -0700

    filled the models, made associations and connected server to data base

--- models/Product.js
+++ models/Product.js
@@ -4,12 +4,49 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      // 10 numbers to the left of decimal and 2 numbers to the right
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      validate:{
+        // validates if its a number
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      // validates if this is a number
+      validate:{
+        isNumeric: true,
+      }
+    },
+    category_id:{
+      type: DataTypes.INTEGER,
+      // product model references the category model through its id which is its primary key
+      references:{
+        model: 'category',
+        key: 'id'
+      },
+    }
+  
   },
   {
     sequelize,
@@ -18,6 +55,7 @@ Product.init(
     underscored: true,
     modelName: 'product',
   }
+
 );
 
 module.exports = Product;
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,28 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // define columns\
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey:true, 
+      autoIncrement:true,
+    },
+    product_id:{
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product', 
+        key: 'id'
+      }
+    },
+    tag_id:{
+      type: DataTypes.INTEGER,
+      // productTag model references the tag model through its id which is its primary key
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
